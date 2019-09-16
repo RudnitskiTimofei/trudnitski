a91fb2204b40e185049cdd747a2701d7ef637d84
RudnitskTimofei <3761544Radiohead>
2019-09-16 19:37:39 +0300

    1 блок

--- src/by/it/trudnitski/JD01/Task2_4.java
+++ src/by/it/trudnitski/JD01/Task2_4.java
@@ -14,24 +14,24 @@ public class Task2_4 {
         System.out.println(" Введите год ");
         int year = sc.nextInt();
 
-        if (day == 28 && month == 2 && year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+        if (day == 28 && month == 2 & year % 4 == 0 & year % 100 == 0 & year % 400 == 0) {
             day++;
             System.out.println(" Дата следующего дня " + day + " : " + month + " : " + year);
 
-        } else if (day == 31 && month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10) {
+        } else if (day == 31 & month == 1 | month == 3 | month == 5 | month == 7 | month == 8 | month == 10) {
             day = 1;
             month++;
             System.out.println(" Дата следующего дня " + day + " : " + month + " : " + year);
 
-        } else if (day == 30 && month == 4 || month == 6 || month == 9 || month == 11) {
+        } else if (day == 30 & month == 4 | month == 6 | month == 9 | month == 11) {
             day = 1;
             month++;
             System.out.println(" Дата следующего дня " + day + " : " + month + " : " + year);
-        } else if (day >= 28 && month == 2) {
+        } else if (day >= 28 & month == 2) {
             day = 1;
             month++;
             System.out.println(" Дата следующего дня " + day + " : " + month + " : " + year);
-        } else if (day == 31 && month == 12) {
+        } else if (day == 31 & month == 12) {
             day = 1;
             month = 1;
             year++;
